26faff2777bd34134a7873701fc82bfb94a20cf2
Ekemini <3k3m1n1@gmail.com>
2022-08-02 15:50:59 -0400

    Final pass on flower spawning
    Flowers now smoothly fade into the scene wherever you're standing, growing over time and then de-spawning after a few seconds. (I found a pretty model for them too)
    
    Also deleted the Concert Hall scene since it's just been sitting there - no point fighting with it

--- Assets/Scripts/FlowerInstance.cs
+++ Assets/Scripts/FlowerInstance.cs
@@ -6,19 +6,28 @@ public class FlowerInstance : MonoBehaviour
 {
     MeshRenderer meshRenderer;
     public float fadeSpeed = 0.4f;
+    public float scaleSpeed = 0.6f;
+
+    Transform anchorPoint;
     
     // Start is called before the first frame update
     void Start()
     {
         meshRenderer = GetComponentInChildren<MeshRenderer>();
+        anchorPoint = this.transform.GetChild(0);
         StartCoroutine(FadeIn());
+        StartCoroutine(Grow());
+        StartCoroutine(FadeOut());
+
+        // despawn flower after a few seconds
+        Destroy(gameObject, 7f);
     }
 
     IEnumerator FadeIn()
     {
-        // fade in the object by lerping the opacity over time
+        // fade in the flower by increasing the opacity over time
 
-        float alphaValue = 0;
+        float alphaValue = 0f;
         Color matColor = meshRenderer.material.color;
 
         while (meshRenderer.material.color.a < 1)
@@ -32,7 +41,33 @@ public class FlowerInstance : MonoBehaviour
 
     IEnumerator Grow()
     {
-        //
-        yield return null;
+        // make the flower "grow" out of the ground by increasing the scale over time
+        float scaleValue = 0f;
+
+        while (anchorPoint.localScale.x < 1)
+        {
+            //
+            scaleValue += scaleSpeed * Time.deltaTime;
+            anchorPoint.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        // start to fade out the flower before despawning
+        yield return new WaitForSeconds(5f);
+
+        float alphaValue = 1f;
+        Color matColor = meshRenderer.material.color;
+
+        while (meshRenderer.material.color.a > 0)
+        {
+            alphaValue -= 0.8f * Time.deltaTime;
+            meshRenderer.material.color = new Color(matColor.r, matColor.g, matColor.b, alphaValue);
+
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/FlowersWhereYouWalk.cs
+++ Assets/Scripts/FlowersWhereYouWalk.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FlowersWhereYouWalk : MonoBehaviour
 {
     GameObject[] visitors;
-    public GameObject flowerPrefab;
+    public GameObject[] flowerPrefabs;
     public float floorY = -0.7f;
     
     // Start is called before the first frame update
@@ -21,13 +21,14 @@ public class FlowersWhereYouWalk : MonoBehaviour
              // find all visitors in scene
             visitors = GameObject.FindGameObjectsWithTag("Visitor");
 
-            // for each one, spawn a flower/plant at their XZ coordinates and Y = the floor
+            // for each one, spawn a flower at their XZ coordinates and Y = the floor
             foreach (GameObject visitor in visitors)
             {
+                GameObject flowerPrefab = flowerPrefabs[Random.Range(0, flowerPrefabs.Length)];
                 Instantiate(flowerPrefab, new Vector3(visitor.transform.position.x + Random.Range(-0.3f, 0.3f), floorY, visitor.transform.position.z + Random.Range(-0.3f, 0.3f)), Quaternion.Euler(0f, Random.Range(-45f, 45f), 0f), this.transform);
             }
 
-            yield return new WaitForSeconds(Random.Range(0.5f, 2));
+            yield return new WaitForSeconds(Random.Range(0.4f, 1.2f));
         }
     }
 }
