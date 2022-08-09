5ea872b87702256fdcc1da87a11fa73e0f6fa71a
Ekemini <3k3m1n1@gmail.com>
2022-08-09 16:32:04 -0400

    Fixed spotlights (coroutines were fighting), documented code

--- Assets/Scripts/ActivateMusicZone.cs
+++ Assets/Scripts/ActivateMusicZone.cs
@@ -12,13 +12,17 @@ public class ActivateMusicZone : MonoBehaviour
     ParticleSystem particles;
     SpriteRenderer spotlight;
     public float fadeSpeed = 0.4f;
+    Coroutine fadeS_in;
+    Coroutine fadeS_out;
 
     bool lightsOn = true;
     ZEDManager zedManager;
 
+
     // Start is called before the first frame update
     void Start()
     {
+        // grab references from scene
         particles = GetComponentInChildren<ParticleSystem>();
         spotlight = transform.GetChild(3).GetComponent<SpriteRenderer>();
         zedManager = GameObject.Find("ZED_Rig_Mono").GetComponent<ZEDManager>();
@@ -50,8 +54,9 @@ public class ActivateMusicZone : MonoBehaviour
 
     IEnumerator FadeSpotlightIn()
     {
-        float alphaValue = 0f;
+        // increment spotlight sprite's alpha over time
 
+        float alphaValue = 0f;
         while (spotlight.color.a < 1)
         {
             alphaValue += fadeSpeed * Time.deltaTime;
@@ -63,8 +68,9 @@ public class ActivateMusicZone : MonoBehaviour
 
     IEnumerator FadeSpotlightOut()
     {
-        float alphaValue = 1f;
+        // decrement spotlight sprite's alpha over time
 
+        float alphaValue = 1f;
         while (spotlight.color.a > 0)
         {
             alphaValue -= 0.8f * Time.deltaTime;
@@ -76,12 +82,16 @@ public class ActivateMusicZone : MonoBehaviour
 
     IEnumerator DimTheScene()
     {
-        int brightnessValue = 100;
+        // set the Camera Brightness variable exposed by the ZED Manager:
+        //          "Brightness of the final real-world image.
+        //          Lower to darken the environment in a realistic way.
+        //          This is a rendering setting that doesn't affect the raw input of the camera."
 
+        int brightnessValue = 100;
         while (zedManager.CameraBrightness > 14)
         {
-            brightnessValue = (int)(brightnessValue - Time.deltaTime);   //brightnessValue--;  //brightnessValue -= Mathf.RoundToInt(1f * Time.deltaTime);
-            zedManager.CameraBrightness = brightnessValue; //studioLights = alphaValue;
+            brightnessValue = (int)(brightnessValue - Time.deltaTime);
+            zedManager.CameraBrightness = brightnessValue;
 
             yield return null;
         }
@@ -93,17 +103,25 @@ public class ActivateMusicZone : MonoBehaviour
         if (other.tag == "Visitor")
         {
             zoneCount++;
-            Debug.Log("Visitor entering zone! # of people inside: " + zoneCount);
+            //Debug.Log("Visitor entering zone! # of people inside: " + zoneCount);
 
             if (zoneCount == 1)
             {
-                particles.Play();
-                StartCoroutine(FadeSpotlightIn());
+                // start particle system
+                particles.Play();   
+                
+                // dim the studio lights, if they haven't been dimmed already
                 if (lightsOn)
                 {
                     StartCoroutine(DimTheScene());
                     lightsOn = false;
                 }
+
+                // fade spotlight on
+                if (fadeS_out != null) { StopCoroutine(fadeS_out); }  // this should solve conflicts where both the fadeIn and fadeOut coroutines are running + therefore stop prematurely
+                fadeS_in = StartCoroutine(FadeSpotlightIn());
+
+                // fade in the music track
                 StartCoroutine(FadeVolume(audioMixer, audioMixerParameter, 0.2f, 1f));    // make sure to type the parameter's name into the inspector! violinVolume, fluteVolume, etc.
                                                                                          // and drag in the audio mixer reference as well
             }
@@ -116,12 +134,18 @@ public class ActivateMusicZone : MonoBehaviour
         if (other.tag == "Visitor")
         {
             zoneCount--;
-            Debug.Log("A visitor exited. # of people remaining: " + zoneCount);
+            //Debug.Log("A visitor exited. # of people remaining: " + zoneCount);
 
             if (zoneCount == 0)
             {
+                // stop generating particles
                 particles.Stop();
-                StartCoroutine(FadeSpotlightOut());
+
+                // fade spotlight off
+                if (fadeS_in != null) { StopCoroutine(fadeS_in); }  // this should solve conflicts where both the fadeIn and fadeOut coroutines are running + therefore stop prematurely
+                fadeS_out = StartCoroutine(FadeSpotlightOut());
+
+                // fade out the music track
                 StartCoroutine(FadeVolume(audioMixer, audioMixerParameter, 0.2f, 0.0001f));
             } 
         }
